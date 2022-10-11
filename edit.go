b925f07f23e809b20c395ff0d8d448d872ea4e80
Emmett Henderson <emmetth@cheesygames.com>
2022-10-11 03:25:14 -0400

    Added Edit fields - not working

--- edit.go
+++ edit.go
@@ -3,12 +3,14 @@ package main
 import (
 	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/emmetth/phonebk/contacts"
 )
 
 type EditModel struct {
-	contact contacts.Contact
+	focus  int
+	inputs []textinput.Model
 }
 
 func (m EditModel) Init() tea.Cmd {
@@ -17,13 +19,60 @@ func (m EditModel) Init() tea.Cmd {
 }
 
 func NewEditModel(contact contacts.Contact) EditModel {
-	return EditModel{contact}
+	m := EditModel{}
+	m.focus = 0
+	m.inputs = make([]textinput.Model, 10)
+
+	for i := range m.inputs {
+		t := textinput.New()
+		t.CharLimit = 40
+		t.Width = 40
+
+		switch i {
+		case 0:
+			t.Placeholder = "First Name"
+			t.Focus()
+			t.SetValue(contact.Fname)
+		case 1:
+			t.Placeholder = "Last Name"
+			t.SetValue(contact.Lname)
+		case 2:
+			t.Placeholder = "Phone"
+			t.SetValue(contact.Phone)
+		case 3:
+			t.Placeholder = "Email"
+			t.SetValue(contact.Email)
+		case 4:
+			t.Placeholder = "Address"
+			t.SetValue(contact.Address)
+		case 5:
+			t.Placeholder = "City"
+			t.SetValue(contact.City)
+		case 6:
+			t.Placeholder = "State"
+			t.SetValue(contact.State)
+		case 7:
+			t.Placeholder = "Zipcode"
+			t.SetValue(contact.Zipcode)
+		case 8:
+			t.Placeholder = "mm/dd/yyyy"
+			t.SetValue(contact.Birthday)
+		case 9:
+			t.Placeholder = "Notes"
+			t.SetValue(contact.Notes)
+		}
+	}
+
+	return m
 }
 
 func (m EditModel) View() string {
 	var sb strings.Builder
-	sb.WriteString("edit screen")
-	sb.WriteString("First Name: " + m.contact.Fname)
+
+	for i := range m.inputs {
+		sb.WriteString(m.inputs[i].View() + "\n")
+	}
+
 	return sb.String()
 }
 
@@ -36,5 +85,6 @@ func (m EditModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, BackCmd()
 		}
 	}
+
 	return m, nil
 }
